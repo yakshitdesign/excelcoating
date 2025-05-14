012ff86b49f521b33dde093690759344260c19e6
Yakshit Patel <68852135+yakshitdesign@users.noreply.github.com>
2025-05-14 08:11:58 +0530

    quick-fix

---
+++ frontend/src/app/about/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+import AboutUs from '@/components/AboutUs';
+
+export default function AboutPage() {
+  return (
+    <div className="flex flex-col gap-4">
+      <AboutUs />
+    </div>
+  );
+}
